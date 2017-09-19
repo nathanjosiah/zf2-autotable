c6be1fce8728a6f2dad6feb8c0bf14c1987f35e9
Nathan Smith <nathanjosiah@gmail.com>
2017-09-19 11:28:40 -0700

    Reenabled dev deps

--- test/Bootstrap.php
+++ test/Bootstrap.php
@@ -1,10 +1,5 @@
 <?php
-class Bootstrap {
-	static function init() {
-		$path = __DIR__ . '/../vendor/autoload.php';
-		if(file_exists($path)) {
-			include_once $path;
-		}
-	}
+$path = __DIR__ . '/../vendor/autoload.php';
+if(file_exists($path)) {
+	include_once $path;
 }
-\Bootstrap::init();
