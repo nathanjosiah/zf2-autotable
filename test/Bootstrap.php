c0fed036036ba97b90206d6cd63c46302f2ccf3a
Nathan Smith <nathanjosiah@gmail.com>
2017-09-19 10:59:22 -0700

    Added autoload

--- test/Bootstrap.php
+++ test/Bootstrap.php
@@ -1,6 +1,10 @@
 <?php
 class Bootstrap {
 	static function init() {
+		$path = __DIR__ . '/../vendor/autoload.php';
+		if(file_exists($path)) {
+			include_once $path;
+		}
 	}
 }
 \Bootstrap::init();
